b32bc6b4fc3ed1b0c7762b620068d1ed246b7b10
Hakimi Maula <muhammad.hakimi.maula.abdul.jalil@prudential.com.my>
2022-03-27 12:52:14 +0800

    readded async storage components

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -4,12 +4,43 @@ import {
 } from 'react-native';
 import DATA from '../../../src/assets/data.json'
 import { StyleSheet, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const MainPage = ({ navigation }) => {
 
     const [data, setData] = useState([])
     const [reload, setReload] = useState(false)
     const [selectedId, setSelectedId] = useState('')
+
+    const storeData = async (value) => {
+        try {
+          const jsonValue = JSON.stringify(value)
+          await AsyncStorage.setItem('stored-item', jsonValue)
+        } catch (e) {
+          console.log('----error', e)
+        }
+      }
+
+    const getData = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('stored-item')
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch(e) {
+            console.log('----error', e)
+        }
+      }
+
+    const setPageData = () => {
+        storeData(DATA)
+        const data = getData()
+        console.log('------get Data')
+        setData(data)
+    }
+
+    useEffect(() => {
+        setPageData()
+    }, [])
+
     const onRefresh = () => {
         setReload(true)
         setData()
