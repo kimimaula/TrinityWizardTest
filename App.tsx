bcb4b20419abea2b18b72839af6c957d37ab976c
Hakimi Maula <muhammad.hakimi.maula.abdul.jalil@prudential.com.my>
2022-03-27 12:12:42 +0800

    add base navigation

--- App.tsx
+++ App.tsx
@@ -1,112 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { Node } from 'react';
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const Stack = createNativeStackNavigator();
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const NavigationBtn = ({props, text}) => {
+    return(
+      <TouchableOpacity onPress={() => {}} >
+        <Text style={styles.btnStyle} {...props}>
+            {text}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+
+
+  const Test = () => {
+    return(
+      <Text>Test</Text>
+    )
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" >
+          <Stack.Screen
+            options={{
+              title: 'Contacts',
+              headerLeft: (props) => (
+                <TouchableOpacity
+                  {...props}
+                  onPress={() => {
+                    // Do something
+                  }}
+                ><Text>search</Text></TouchableOpacity>
+              ),
+              headerRight: (props) => (
+                <TouchableOpacity
+                  {...props}
+                  onPress={() => {
+                    // Do something
+                  }}
+                ><Text>add</Text></TouchableOpacity>
+              ),
+            }}
+           name="Home" component={Test} />
+          <Stack.Screen
+            options={{
+              title: '',
+              headerLeft: (props) => (
+                <NavigationBtn props={props} text='Cancel'/>
+              ),
+              headerRight: (props) => (
+                <NavigationBtn props={props} text='Save'/>
+              ),
+              }}
+              name="Edit" component={Test} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  btnStyle: {
+    color: '#ff8c00'
+  }
 });
 
 export default App;
